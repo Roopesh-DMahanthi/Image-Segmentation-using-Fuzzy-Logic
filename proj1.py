9747242ff6f9f5996bf453f67b4d3d127a2d5889
Roopesh-DMahanthi <deepthimahanthi.roopesh@gmail.com>
2019-11-07 15:07:38 +0530

    commit 2

--- proj1.py
+++ proj1.py
@@ -21,6 +21,7 @@ class ImgSeg:
         Ix1=cons.Ix[:,:]/xMax
         yMax=np.max(cons.Iy)
         Iy1=cons.Iy[:,:]/yMax
+        I=np.empty(Ix1.shape)
         #print(np.unique(Ix1))
         #print(np.unique(Iy1))
         IxMat=ctrl.Antecedent(np.arange(0,1,0.001), 'Ix')
@@ -30,24 +31,40 @@ class ImgSeg:
         IxMat['W']=fuzz.trimf(IxMat.universe, [0.2, 1, 1])
         IyMat['B']=fuzz.trimf(IyMat.universe, [0, 0, 0.8])
         IyMat['W']=fuzz.trimf(IyMat.universe, [0.2, 1, 1])
+        '''
         IxMat.view()
         IyMat.view()
+        '''
         Pic['B']=fuzz.trimf(Pic.universe, [0, 0, 0.8])
         Pic['W']=fuzz.trimf(Pic.universe, [0.2, 1, 1])
         rule1 = ctrl.Rule(IxMat['B'] & IyMat['B'], Pic['B'])
         rule2 = ctrl.Rule(IxMat['W'], Pic['W'])
         rule3 = ctrl.Rule(IyMat['W'], Pic['W'])
-        rule1.view()
-        rule2.view()
-        rule3.view()
         
         Edge_ctrl = ctrl.ControlSystem(rules=[rule1, rule2, rule3])
         Edging = ctrl.ControlSystemSimulation(Edge_ctrl)
+        '''
         Edging.input['Ix'] = 0.2
         Edging.input['Iy'] = 0.8
         Edging.compute()
         print(Edging.output['Picture'])
         Pic.view(sim=Edging)
+        '''
+        mainImg=cons.imArr.copy()
+        for i in range(Ix1.shape[0]):
+            for j in range(Ix1.shape[1]):
+                Edging.input['Ix'] = Ix1[i,j]
+                Edging.input['Iy'] = Iy1[i,j]
+                Edging.compute()
+                I[i,j]=Edging.output['Picture']*255
+                if(I[i,j]>130):
+                    mainImg[i,j]=0
+                    
+        print(np.unique(I))
+        #plt.imshow(I,cmap = plt.get_cmap('gray'))
+        Image.fromarray(I).show()
+        Image.fromarray(mainImg).show()
+        
         
 
 
@@ -57,18 +74,37 @@ class ImgSeg:
     def calcs(cons):
         cons.Ix=np.empty(cons.imArr.shape)
         cons.Iy=np.empty(cons.imArr.shape)
+        '''
         for i in range(1,cons.imArr.shape[1]):
             cons.Ix[:,i]=abs(cons.imArr[:,i]-cons.imArr[:,i-1])
         for i in range(1,cons.imArr.shape[0]):
             cons.Iy[i,:]=abs(cons.imArr[i,:]-cons.imArr[i-1,:])
+        
+        for i in range(1,cons.imArr.shape[0]):
+            for j in range(1,cons.imArr.shape[1]):
+                cons.Ix[i,j]=(cons.imArr[i,j]-cons.imArr[i,j-1])
+                cons.Iy[i,j]=(cons.imArr[i,j]-cons.imArr[i-1,j])
+        '''
+        for i in range(1,cons.imArr.shape[0]):
+            for j in range(1,cons.imArr.shape[1]):
+                if(abs(cons.imArr[i,j]-cons.imArr[i,j-1])>0):
+                    cons.Ix[i,j]=255
+                if(abs(cons.imArr[i,j]-cons.imArr[i-1,j])>0):
+                    cons.Iy[i,j]=255
+        print(np.unique(cons.imArr))
+        #print(np.unique(cons.Ix))
+        print(np.unique(cons.Iy))
+        
+        '''
         plt.subplot(1,2,1)
         plt.imshow(cons.Ix,cmap = plt.get_cmap('gray'))
         plt.subplot(1,2,2)
         plt.imshow(cons.Iy,cmap = plt.get_cmap('gray'))
+        '''
         imx=Image.fromarray(cons.Ix)
         imy=Image.fromarray(cons.Iy)
-        #imx.show()
-        #imy.show()
+        imx.show()
+        imy.show()
         cons.fis()
     
     def Convert2Gmat(cons):
@@ -76,7 +112,7 @@ class ImgSeg:
         img=Image.open(cons.addr).convert('L')
         imArr=np.asarray(img)
         #print(imArr.shape)
-        #img.show()
+        img.show()
         #plt.imshow(imArr,cmap = plt.get_cmap('gray'))
         cons.imArr=imArr
         cons.calcs()
