d0c3511af23c60c055a46eddce946d8d4a7b988c
Roopesh-DMahanthi <deepthimahanthi.roopesh@gmail.com>
2019-11-18 11:38:07 +0530

    Final Commit

--- projtest.py
+++ projtest.py
@@ -10,27 +10,34 @@ Created on Fri Nov  8 20:14:10 2019
 """
 Created on Sat Oct 26 15:51:51 2019
 
-@author: Roopesh
+@author: Group-45,46,47,48
 """
 import cv2
 from PIL import Image
 import numpy as np
 np.seterr(over='ignore')
-import matplotlib.pyplot as plt
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
-
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+        # Print New Line on Complete
+        if iteration == total: 
+            print()
 
 class ImgSeg:
     
     def disp(cons):
-        cImg=Image.open(cons.addr)
+        cImg=Image.open('temp.png')
         for i in range(cons.edgeImg.shape[0]):
             for j in range(cons.edgeImg.shape[1]):
                 if(cons.edgeImg[i,j]>70):
                     cImg.putpixel((j,i),(0,0,0,255))
         cImg.show()
+        cImg.save("finalimg.png")
     
     def fis(cons):
         Ix1=cons.Ix[:,:]/255
@@ -59,28 +66,24 @@ class ImgSeg:
         
         Edge_ctrl = ctrl.ControlSystem(rules=[rule1, rule2, rule3])
         Edging = ctrl.ControlSystemSimulation(Edge_ctrl)
-        '''
-        Edging.input['Ix'] = 0.2
-        Edging.input['Iy'] = 0.8
-        Edging.compute()
-        print(Edging.output['Picture'])
-        Pic.view(sim=Edging)
         
-        cImg=Image.open(cons.addr)
-        mainImgC=np.asarray(cImg).copy()
-        '''
-        
+        ind=0
+        l=Ix1.shape[0]*Ix1.shape[1]
         for i in range(Ix1.shape[0]):
             for j in range(Ix1.shape[1]):
+                printProgressBar(ind+1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+                ind+=1
                 Edging.input['Ix'] = Ix1[i,j]
                 Edging.input['Iy'] = Iy1[i,j]
                 Edging.compute()
                 I[i,j]=round(Edging.output['Picture'] * 255)
                 
                     
-        print(np.unique(I))
+        #print(np.unique(I))
         #plt.imshow(I,cmap = plt.get_cmap('gray'))
-        Image.fromarray(I).show()
+        edgImg=Image.fromarray(I).convert('RGB')
+        edgImg.show()
+        edgImg.save('edgedout.png')
         cons.edgeImg=I
         
         #Image.fromarray(mainImg).show()
@@ -108,7 +111,7 @@ class ImgSeg:
         imy.show()
         '''
         
-       
+      
     def calcs2(cons):
         cons.IxRL=np.zeros(cons.imArr.shape)
         cons.IyDU=np.zeros(cons.imArr.shape)
@@ -116,12 +119,7 @@ class ImgSeg:
         cons.IyUD=np.zeros(cons.imArr.shape)
         cons.Ix=np.zeros(cons.imArr.shape)
         cons.Iy=np.zeros(cons.imArr.shape)
-        '''
-        for i in range(1,cons.imArr.shape[1]):
-            cons.Ix[:,i]=abs(cons.imArr[:,i]-cons.imArr[:,i-1])
-        for i in range(1,cons.imArr.shape[0]):
-            cons.Iy[i,:]=abs(cons.imArr[i,:]-cons.imArr[i-1,:])
-        '''
+        
         for i in range(0,cons.imArr.shape[0]):
             for j in range(0,cons.imArr.shape[1]):
                 if(j!=0):
@@ -144,13 +142,6 @@ class ImgSeg:
                 else:
                     cons.Iy[i,j]=cons.IyDU[i,j]
         '''
-        for i in range(1,cons.imArr.shape[0]):
-            for j in range(1,cons.imArr.shape[1]):
-                if(abs(cons.imArr[i,j]-cons.imArr[i,j-1])>0):
-                    cons.Ix[i,j]=255
-                if(abs(cons.imArr[i,j]-cons.imArr[i-1,j])>0):
-                    cons.Iy[i,j]=255
-        
         print(np.unique(cons.imArr))
         print(np.unique(cons.Ix))
         print(np.unique(cons.Iy))
@@ -161,7 +152,7 @@ class ImgSeg:
         plt.subplot(1,2,2)
         plt.imshow(cons.Iy,cmap = plt.get_cmap('gray'))
         
-        imx=Image.fromarray(cons.Ix)
+            imx=Image.fromarray(cons.Ix)
         imy=Image.fromarray(cons.Iy)
         imx.show()
         imy.show()
@@ -169,10 +160,19 @@ class ImgSeg:
     
     def Convert2Gmat(cons):
         #print(cons.addr)
-        img=Image.open(cons.addr).convert('L')
+        img=Image.open(cons.addr)
+        width, height = img.size
+        while(width*height>90000) :
+            width//=2
+            height//=2
+            img = img.resize((width, height)) 
+        print(width,height)
+        img.save('temp.png')
+        img.show()
+        img=img.convert('L')
         imArr=np.asarray(img)
         #print(imArr.shape)
-        img.show()
+        
         #plt.imshow(imArr,cmap = plt.get_cmap('gray'))
         cons.imArr=imArr
         
@@ -181,7 +181,7 @@ class ImgSeg:
     def __init__(cons,addr):
         cons.addr=addr
         cons.Convert2Gmat()
-        print("Primary Image to Gray-Scale Conversion Successful.....")
+        print("Primary Image Resizing & Gray-Scale Conversion Successful.....")
         cons.calcs()
         print("Gradient Details Extracted Successfully.....")
         print("Applying Fuzzy Interface System on Gradients......(Wait For a While)")
@@ -193,5 +193,5 @@ class ImgSeg:
         
         
         
-        
-x=ImgSeg('result.png')
+file=input("Enter a valid image name - ")
+x=ImgSeg(file)
